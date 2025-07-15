03533223152312e9669576256d8af0ca40d0a22b
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-07-15 08:59:05 +0600

    ok

--- ploting.py
+++ ploting.py
@@ -1,22 +1,51 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# first plot
+# draw a line in a diagram
 xpoints = np.array([-11,8])
 ypoints = np.array([5,10])
 
 plt.plot(xpoints, ypoints)
 plt.show()
 
- # second plot
+ # plotting without line
 xpoints = np.array([1, 8])
 ypoints = np.array([3, 10])
 
 plt.plot(xpoints, ypoints, 'o')
 plt.show()
 
-# third plot
-xpoints = np.array([1,2,6,8])
-ypoints = np.array([3,8,1,10])
-plt.plot(xpoints, ypoints)
+# plotting multiple points
+xpoin = np.array([1,2,6,8])
+ypoin = np.array([3,8,1,10])
+
+plt.plot(xpoin, ypoin)
 plt.show()
+
+# plotting without x-points:
+
+ypoints = np.array([3,8,1,10,5,7])
+
+plt.plot(ypoints)
+plt.show()
+
+#matplotlib markers
+
+mpoint = np.array([3,8,1,10,5,12])
+
+plt.plot(mpoint, marker='H')  # there are so many marker read them from w3 school
+plt.show()
+
+# formating string fmt: we can also use the shortcut string notation parameter to specify the marker
+
+ypints  = np.array([3,8,1,10,5,12])
+plt.plot(ypints,'H--r') # there are so many option for color and make and which type line I want
+plt.show()
+
+# use marker size and color
+
+ypoints = np.array([3,8,1,10,5,12])
+
+plt.plot(ypoints,'o-.m',ms = 49,mfc='r',mec='b')
+plt.show()
+
