56748e694b993c378956002c86fab5b3ca53a02e
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-07-18 19:40:26 +0600

    ok

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="prosen-pythonlib",  # Must be unique on PyPI
-    version="0.1.0",
+    name="prosen-pythonlib",
+    version="0.1.1",  # Make sure to increment this from the previous version
     packages=find_packages(),
     author="Prosenjit Mondol",
+    author_email="ug2102049@cse.pstu.ac.bd",  # ✅ Matching GitHub email
     description="A simple greeting package",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
