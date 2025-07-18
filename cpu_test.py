f51015f95340bcb16ac1762d9d0553af80c81e27
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-07-18 22:47:01 +0600

    7/18/25

--- cpu_test.py
+++ cpu_test.py
@@ -8,7 +8,7 @@ print(f"Number of CPUs: {mp.cpu_count()}")
 def random_square(seed):
     np.random.seed(seed)
     random_num=np.random.randint(0,10)
-    return random_num
+    return random_num**2
 
 t0=time.time()
 results=[]
@@ -19,11 +19,25 @@ print(f'Serial Execution time: {t1-t0}')
 
 
 # parallel version
-t0=time.time()
-n_cpu=mp.cpu_count()
 
-pool=mp.Pool(processes=n_cpu)
-results = pool.map(random_square, range(1000000))
-t1=time.time()
-print(f'Parallel CPU Execution time: {t1-t0}')
+def random_square(x):
+    return x * x
+
+if __name__ == '__main__':
+    t0 = time.time()
+    n_cpu = mp.cpu_count()
+
+    # Create a multiprocessing Pool
+    pool = mp.Pool(processes=n_cpu)
+
+    # Map the function to a range of values
+    results = pool.map(random_square, range(1000000))
+
+    # Close the pool and wait for the work to finish
+    pool.close()
+    pool.join()
+
+    t1 = time.time()
+    print(f'Parallel CPU Execution time: {t1 - t0}')
+
 
