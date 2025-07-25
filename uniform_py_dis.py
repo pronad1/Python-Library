4ca7b035cbf25475754430f06865eb7d53d64398
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-07-25 15:15:46 +0600

    ok:

--- uniform_py_dis.py
+++ uniform_py_dis.py
@@ -6,5 +6,21 @@ import seaborn as sns
 
 x=random.uniform(size=(2,3))
 print(x)
-sns.displot(random.uniform(size=1000),kind="kde")
+# Visualization of Uniform Distribution
+sns.displot(random.uniform(size=1000), kind="kde")
+plt.show()
+
+# Logistic distribution is used to describe growth. loc-mean, scale-standard deviation, size-the shape
+x=random.logistic(1,2,(2,3))
+print(x)
+# visualization of logistic
+sns.displot(random.logistic(size=(500)), kind="kde")
+plt.show()
+
+# difference between logistic and uniform
+data = {
+  "normal": random.normal(scale=2, size=1000),
+  "logistic": random.logistic(size=1000)
+}
+sns.displot(data, kind="kde")
 plt.show()
